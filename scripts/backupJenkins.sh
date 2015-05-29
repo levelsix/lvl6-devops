689af745993ebaa2b3c35822e9aaaeb781b949f9
bluestix <kelly@dekayd.com>
2015-05-28 17:22:22 -0700

    modify ansible scripts

---
+++ scripts/backupJenkins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo Backing up Jenkins
+
+DATE=`date +%Y-%m-%d-%H-%M-%S`
+backupDir=/opt/jenkinsBackups
+backupName=$backupDir/jenkins-backup-$DATE.7z
+jenkinsDir=/var/lib/jenkins
+s3Bucket=lvl6-build-backups
+
+7z a "$backupName" "$jenkinsDir"
+
+
+echo Removing old backups
+
+find "$backupDir/*.7z" -type f -mtime +60 -exec rm {} \;
+
+
+echo Syncing to S3
+
+aws s3 sync s3://$s3bucket/jenkins/ $backupDir/ --acl private --delete
+
+
+
