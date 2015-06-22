38c43163eaf050a73759618d07d031baa53ace2a
bluestix <kelly@dekayd.com>
2015-06-22 15:21:13 -0700

    changes for curator and jenkins backups

--- scripts/backupJenkins.sh
+++ scripts/backupJenkins.sh
@@ -8,7 +8,7 @@ backupName=$backupDir/jenkins-backup-$DATE.7z
 jenkinsDir=/var/lib/jenkins
 s3Bucket=lvl6-build-backups
 
-7z a "$backupName" "$jenkinsDir"
+7z a "$backupName" "$jenkinsDir" -xr!target -xr!.git -xr!.m2 -xr!*.jar -xr!*.war
 
 
 echo Removing old backups
@@ -16,9 +16,9 @@ echo Removing old backups
 find $backupDir -iname "*.7z" -type f -mtime +60 -exec rm {} \;
 
 
-echo Syncing to S3
+echo Syncing to S3 with command: 
+echo aws s3 sync $backupDir/ s3://$s3Bucket/jenkins/ --acl private --delete --region us-west-2
 
-aws s3 sync s3://$s3bucket/jenkins/ $backupDir/ --acl private --delete --region us-west-2
-
+aws s3 sync $backupDir/ s3://$s3Bucket/jenkins/ --acl private --delete --region us-west-2
 
 
